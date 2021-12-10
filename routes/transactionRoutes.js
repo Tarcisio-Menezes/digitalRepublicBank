0bc3e595bf890b791dd71cb9c10f9d65c0e665e2
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-10 18:56:25 -0300

    adiciona cashDeposit em routes de tranference

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/transactionControllers');
 
 router.post('/transaction', controller.cashTranference);
+router.post('/transaction/deposit', controller.cashDeposit);
 
 module.exports = router;
