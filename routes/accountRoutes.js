e75f582eab23696e1f274ace318b3ed4dc98deda
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-10 13:42:51 -0300

    adiciona accountRegister em routes

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/accountControllers');
 
 router.get('/account/:id', controller.searchAccountById);
+router.get('/account', controller.accountRegister);
 
 module.exports = router;
