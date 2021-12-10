1c165cffebdafd0366d29914be9198b335c7828d
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-10 13:46:32 -0300

    adiciona erros de accountRegister em middleware

--- middlewares/accountMiddlewares.js
+++ middlewares/accountMiddlewares.js
@@ -5,5 +5,13 @@ module.exports = (err, _req, res, _next) => {
     return res.status(404).json({ message: 'Account not found' });
   }
 
+  if (err.code === 'accountAlreadyExists') {
+    return res.status(409).json({ message: 'Account Already Exists' });
+  }
+
+  if (err.code === 'invalidFields') {
+    return res.status(401).json({ message: 'Invalid name or cpf' });
+  }
+
   return res.status(500).json({ message: 'Critical Error :(' });
 };
