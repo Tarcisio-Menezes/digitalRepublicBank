11e52326b96fee737c6295d2449c6630e95e909f
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-10 16:44:27 -0300

    adiciona searchAccountByFullName em routes

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const controller = require('../controllers/accountControllers');
 
 router.get('/account/:id', controller.searchAccountById);
+router.get('/account/search-by-name/:fullName', controller.searchAccountByFullName);
 router.post('/account', controller.accountRegister);
 
 module.exports = router;
