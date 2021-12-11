51bafce3d79ca6a89d1f5de75dc8d801d4a34c79
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-11 13:33:54 -0300

    adiciona getAllAccount em routes

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/accountControllers');
 
 router.get('/account/:id', controller.searchAccountById);
 router.get('/account/search-by-name/:fullName', controller.searchAccountByFullName);
+router.get('/account', controller.getAllAccount);
 router.post('/account', controller.accountRegister);
 
 module.exports = router;
